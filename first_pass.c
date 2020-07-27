be8d87d75d7d7791badbf8f00a42a008356ef700
Lior Merkel <markofkain0@gmail.com>
2020-07-27 11:02:35 +0300

    validation functions

--- first_pass.c
+++ first_pass.c
@@ -49,7 +49,7 @@ check if saved keyword
             }
         }
         if(line[index1] == '.'){
-            switch(isValidAsmOpt(line, index1+1, index2, lineCounter)){
+            switch(isValidAsmOpt(line, index1, index2, lineCounter)){
                 case ERROR:
                     errorFlag = TRUE;
                     break;
@@ -71,9 +71,52 @@ check if saved keyword
 
     }
 }
-int isKeyword(char line[],int index1,int index2)
+/*checks if a given word is a saved keyword used by the assembly language.
+ *uses length of the word to avoid unnecessary strcmp.*/
+int isKeyword(char* word, int wordLen)
 {
+	/*TODO: Asaf needs to look to confirm that I didn't miss any.*/
+	switch(wordLen){
+		case 3:
+			if(!strcmp(word, "mov") ||
+			   !strcmp(word, "cmp") ||
+			   !strcmp(word, "add") ||
+			   !strcmp(word, "sub") ||
+			   !strcmp(word, "lea") ||
+			   !strcmp(word, "clr") ||
+			   !strcmp(word, "not") ||
+			   !strcmp(word, "inc") ||
+			   !strcmp(word, "dec") ||
+			   !strcmp(word, "jmp") ||
+			   !strcmp(word, "bne") ||
+			   !strcmp(word, "jsr") ||
+			   !strcmp(word, "red") ||
+			   !strcmp(word, "prn") ||
+			   !strcmp(word, "rts"))
+				return TRUE;
+			break;
+		case 4:
+			if(!strcmp(word, "stop"))
+					return TRUE;
+			break;
+		case 5:
+			if(!strcmp(word, ".data"))
+				return TRUE;
+			break;
+		case 6:
+			if(!strcmp(word, ".entry"))
+				return TRUE;
+			break;
+		case 7:
+			if(!strcmp(word, ".string") ||
+			   !strcmp(word, ".extern"))
+				return TRUE;
+		default:
+			break;
+	}
+	return FALSE;
 }
+/*adjusts indicies to bound a word*/
 void getWord(char* line, int* i, int* index1, int* index2)
 {
     while(isspace(line[*i])) 
@@ -83,25 +126,29 @@ void getWord(char* line, int* i, int* index1, int* index2)
         ++*i;
     *index2 = *i-1;
 }
-/*Validates that word is a valid assembly instruction*/
+/*asserts that word is a valid assembly instruction.
+ *Must be one of the following: .data, .string, .entry, .extern*/
 int isValidAsmOpt(char* line, int index1, int index2, int lineCounter)
 {
-    char opt[MAXOPTSIZE];
+    char asmOpt[MAXOPTSIZE];
     int wordLen = index2 - index1 + 1;
     if(wordLen > MAXOPTSIZE){ /*max opt size*/
         printf("ERROR: invalid assembler instruction at line: %d\n", lineCounter);
         return ERROR;
     }
-    strncpy(opt, line+index1, wordLen);
-    if(!strcmp(opt, "data"))
+    strncpy(asmOpt, line+index1, wordLen);
+    if(!strcmp(asmOpt, ".data"))
         return DATA;
-    if(!strcmp(opt, "string"))
+    if(!strcmp(asmOpt, ".string"))
         return STRING;
-    if(!strcmp(opt, "entry"))
+    if(!strcmp(asmOpt, ".entry"))
         return ENTRY;
-    if(!strcmp(opt, "extern"))
+    if(!strcmp(asmOpt, ".extern"))
         return EXTERN;
 }
+/*asserts that a given label is a valid label.
+ *a label can have up to 31 characters, begin with a letter 
+ *from the range <a-z>/<A-Z> and must be fully alphanumeric*/
 int isValidLabel(char line[],int index1,int index2,int lineCounter)
 {
     int j;
@@ -120,11 +167,14 @@ int isValidLabel(char line[],int index1,int index2,int lineCounter)
         return 0;
         }
     }
-    if(iskeyword(line, index1, index2-1)){/*TODO strncp*/
-        printf("ERROR: label canno't be a saved keyword; at line: %d\n",lineCounter);
+    strncpy(word, line+index1, MAXLABELSIZE);
+    if(iskeyword(word, index2-index1+1)){
+        printf("ERROR: label cannot be a saved keyword; at line: %d\n",lineCounter);
         return 0;
     }
-    if(exists(line, index1,)){/*TODO strncp*/
+    /*TODO:Need to decide where we declare our linked list and wheter we pass it on here
+     * or make it global. probably the former?*/
+    if(exists(word)){
         printf("ERROR: Duplicate label definition ; at line: %d\n",lineCounter);
         return 0;
     }
