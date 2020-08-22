c0044a0084504fe89dd622c4f7af095eaaf0c705
Lior Merkel <markofkain0@gmail.com>
2020-08-22 19:39:49 +0300

    Comments

--- first_pass.c
+++ first_pass.c
@@ -8,34 +8,42 @@
 #include "first_pass.h"
 #include "constants.h"
 
+/*First pass of our assembler.
+ *here we check for syntax errors, add our programs'
+ *labels to the symbol table and adjust our instruction
+ *and data counters to later allocate memory for memory pictures.
+ */
 int firstPass(FILE* fp, int* dataCounter, int* instCounter)
 {
     char line[MAX_LINE_LENGTH + 2];
     Token word;/*Struct holding current word and its' length*/
     int lineCounter = 0;
-    int i = 0, lineLen;
+    int lineLen;
     /*TODO:Still think we need to have a cleaner way to describe going over the line.
      * maybe bundling index1 with index2 and call it something like WordBound(similar to Token
      * but just for bounding. maybe even have a Token and TokenCopy*/
-    int index1, index2;
+    int i = 0, index1, index2;
     char errorFlag = FALSE;
-    char labelFlag;
-    char labelTemp[MAXLABELSIZE+1]; /*for adding the label to the symbol table later*/
+
+    char labelFlag;/*denotes if a labe needs to be added for current line*/
+    char labelTemp[MAXLABELSIZE+1];/*for adding the label to the symbol table later*/
     
+    /*reset IC and DC*/
     *dataCounter = 0;
     *instCounter = 0;
+
     while(fgets(line, MAX_LINE_LENGTH + 2, fp)){/*Parsing lines*/
         int cmdIndex;/*Index holding place inside CMD array*/
         i = 0; 
-        labelFlag = FALSE;
-        ++lineCounter;
+        labelFlag = FALSE;/*new line, reset labelFlag*/
+        ++lineCounter;/*counting line number*/
         lineLen = strlen(line);
 
         if(line[lineLen-1] != '\n' && !feof(fp)){/*line longer than 80 characters*/
             char c;
             printf("ERROR:%d: Line exceeds maximum length of %d characters\n" ,lineCounter,MAX_LINE_LENGTH);
             errorFlag = TRUE;
-            while((c = fgetc(fp)) != '\n' && c != EOF);
+            while((c = fgetc(fp)) != '\n' && c != EOF);/*consume remaining input after 81+ line*/
             continue;
         }
 
@@ -44,101 +52,110 @@ int firstPass(FILE* fp, int* dataCounter, int* instCounter)
             continue;
         }
 
+        /*set indicies to point to the next word in line
+         *and store said word*/
         getWord(line, &i, &index1, &index2);
         storeWord(&word, &line[index1], index2-index1+1);
 
-        if(line[index2] == ':'){/*Label*/
+        if(line[index2] == ':'){/*if label*/
+
+            /*adjusting word.currentWord to call validation function*/
             word.len -= 1;/*excluding ':'*/
             word.currentWord[word.len] = '\0';
 
-            if (isValidLabel(word.currentWord, word.len, lineCounter)){
+            if(isValidLabel(word.currentWord, word.len, lineCounter)){/*if we've read a valid label*/
                 labelFlag = TRUE;
-                strncpy(labelTemp, word.currentWord, word.len);
+                strncpy(labelTemp, word.currentWord, word.len);/*save label to add to symbol table*/
                 labelTemp[word.len] = '\0';
 
-                getWord(line, &i, &index1, &index2);
+                getWord(line, &i, &index1, &index2);/*next word*/
                 storeWord(&word, &line[index1], index2-index1+1);
-                if(line[index1] == '\0'){
+                if(line[index1] == '\0'){/*if no more words on line*/
                     printf("ERROR:%d: empty label definition \n", lineCounter);                  
                     errorFlag = TRUE;
                     continue;
                 }
             }
-            else{
+            else{/*not a valid label definition*/
                 errorFlag = TRUE;
                 continue; 
             }
-        }
+        }/*if label*/
 
-        if(line[index1] == '.'){
+        if(line[index1] == '.'){/*if assembly opt*/
 
+            /*if a valid assembly opt*/
             int asmOpt = isValidAsmOpt(word.currentWord, lineCounter);
             if(asmOpt == ERROR){
                 errorFlag = TRUE;
                 continue;
             }
 
-            else if(asmOpt == DATA){
-                int dataArgs = validateData(line, i, lineCounter);
-                if(dataArgs == -1){
+            else if(asmOpt == DATA){/*.data*/
+                int dataArgs = validateData(line, i, lineCounter);/*count number of arguments*/
+                if(dataArgs == -1){/*invalid data*/
                     errorFlag = TRUE;
                     continue;
                 }
-                if(labelFlag){
-                    if(!exists(labelTemp))
-                        addLabel(labelTemp, TRUE, FALSE, *dataCounter);
-                    else{
+                if(labelFlag){/*label at the beginning of the line*/
+                    if(!exists(labelTemp))/*not already declared*/
+                        addLabel(labelTemp, TRUE, FALSE, *dataCounter);/*add it to symbol table*/
+                    else{/*label already exists*/
                         printf("ERROR:%d: Duplicate label definition\n" ,lineCounter);
                         errorFlag = TRUE;
                         continue; 
                     }
                 }
-                *dataCounter += dataArgs;
+                *dataCounter += dataArgs;/*add number of arguments of .data to DC*/
             }
 
-            else if(asmOpt == STRING){
+            else if(asmOpt == STRING){/*.string*/
                 int strLen = validateString(line, index2+1, lineLen-1, lineCounter);
-                if(strLen == -1){
+                if(strLen == -1){/*invalid string*/
                     errorFlag = TRUE;
                     continue;
                 }
-                if(labelFlag){
-                    if(!exists(labelTemp))
-                        addLabel(labelTemp, TRUE, FALSE, *dataCounter);
-                    else{
+                /*TODO: put if(labelflag) at the end? we have duplication above*/
+                if(labelFlag){/*label at the beginning of the line*/
+                    if(!exists(labelTemp))/*not already declared*/
+                        addLabel(labelTemp, TRUE, FALSE, *dataCounter);/*add it to symbol table*/
+                    else{/*label already exists*/
                         printf("ERROR:%d: Duplicate label definition\n" ,lineCounter);
                         errorFlag = TRUE;
                         continue; 
                     }
                 }
-                *dataCounter += strLen;
+                *dataCounter += strLen;/*add number of characters in .string to DC*/
             }
 
-            else if(asmOpt == ENTRY){
+            else if(asmOpt == ENTRY){/*.entry*/
+                /*entry is checked on second pass*/
             }
 
-            else if(asmOpt == EXTERN){
+            else if(asmOpt == EXTERN){/*.extern*/
+                /*get label name*/
                 getWord(line, &i, &index1, &index2);
                 storeWord(&word, &line[index1], index2-index1+1);
-                if (isValidLabel(word.currentWord, word.len, lineCounter)){
-                    getWord(line, &i, &index1, &index2);
-                    if(line[index1]=='\0'){/*No text after label*/
-                        if(exists(word.currentWord)&&!isExtern(word.currentWord)){
+
+                if(isValidLabel(word.currentWord, word.len, lineCounter)){
+                    getWord(line, &i, &index1, &index2);/*check for extranous text*/
+                    if(line[index1]=='\0'){/*no text after label*/
+                        if(exists(word.currentWord)&&!isExtern(word.currentWord)){/*if there's another non-extern decleration*/
                             printf("ERROR:%d: Duplicate label definition\n" ,lineCounter);
                             errorFlag = TRUE;
-                            continue;  
+                            continue;
                         }
-                        else{    
+                        else{
                             addLabel(word.currentWord, FALSE, TRUE, 0);
                         }
                     }
-                    else{
+                    else{/*extra text after label name*/
                         printf("ERROR:%d: extranous text after label\n" ,lineCounter);
                         errorFlag = TRUE;
                         continue;
                     }
                 }
-                else{
+                else{/*invalid label name*/
                     errorFlag = TRUE;
                     continue;
                 }
@@ -147,61 +164,75 @@ int firstPass(FILE* fp, int* dataCounter, int* instCounter)
 
         else if((cmdIndex = isOp(word.currentWord)) != -1){/*Operator*/
             int start = i;/*index pointing to first operand*/
-            int wordIdx;
+            int wordIdx;/*index pointing to current operand*/
             int commaIndex;/*index pointing to comma*/
             int params = CMD[cmdIndex].numParams;/*number of operands for the command*/
-            if(labelFlag){
-                if(!exists(labelTemp)){
+
+            if(labelFlag){/*label at the beginning of the line*/
+                if(!exists(labelTemp)){/*not already declared*/
                     addLabel(labelTemp, FALSE, FALSE, *instCounter);
                 }
-                else{
+                else{/*exists already*/
                     printf("ERROR:%d: Duplicate label definition\n", lineCounter);
                     errorFlag = TRUE;
                     continue;
                 }
             }
-            ++*instCounter;/*Count operand*/
-            if(!validateOpNum(line, &i, cmdIndex, lineCounter)){
+
+            ++*instCounter;/*count operand in IC*/
+            if(!validateOpNum(line, &i, cmdIndex, lineCounter)){/*proper number of operands given to the command*/
                 errorFlag = TRUE;
                 continue;
             }
-            if((commaIndex = validateCommas(line, i, cmdIndex, lineCounter)) == -1){
+            if((commaIndex = validateCommas(line, i, cmdIndex, lineCounter)) == -1){/*proper number give to the command*/
                 errorFlag = TRUE;
                 continue;
             }
 
-            while(params){
-                wordIdx = (params == MAXPARAM) ? commaIndex+1 : start;/*According to number of params, let index point to word*/
-                while(isspace(line[wordIdx]))
+            while(params){/*while we're processing operands*/
+                wordIdx = (params == MAXPARAM) ? commaIndex+1 : start;/*according to number of params, let index point to operand*/
+                while(isspace(line[wordIdx]))/*skip whitespace*/
                     ++wordIdx;
-                if(!singleToken(&line[wordIdx], params, lineCounter)){/*If there's more than one token/no token*/
+                if(!singleToken(&line[wordIdx], params, lineCounter)){/*if there's more than one token/no token*/
                     errorFlag = TRUE;
                     break;
                 }
 
+                /*verify proper operands for command and adjust instruction counter accordingly*/
                 if(!verifyOperand(&line[wordIdx], &CMD[cmdIndex], params, instCounter, lineCounter)){
                     errorFlag = TRUE;
                     break;
                 }
 
-                --params;/*Decrement params to get to previous operand*/
+                --params;/*decrement params to get to first operand(if exists)*/
             }/*While end*/
+
         }/*else if Op*/
-        else{
+
+        else{/*Not a label, asmOpt or operator*/
             printf("ERROR:%d: unrecognized instruction \"%s\" \n", lineCounter, word.currentWord);
             errorFlag = TRUE;
             continue;
         }
     }/*While fgets*/
+
+    /*=====================================================================================*/
+    /*Finished parsing the file*/
+
+    /*if no errors found on first pass and total number of assembly instructions
+     *are within memory size*/
     if(!errorFlag && *instCounter+*dataCounter+STARTADDRESS-1 > MAXADDRESS){
         printf("ERROR: Program exceeds maximum address space of %d\n", MAXADDRESS);
         errorFlag = TRUE;
     }
-    return !errorFlag;
+    return !errorFlag;/*return wheter or not we had an error on first pass*/
 }
+/*Checks the syntax, type and usage of a given operand for a given operator,
+ *reports error if found any and adjusts IC according to operand.
+ */
 int verifyOperand(const char* line, const COMMANDS* cmd, int params, int* instCounter, int lineCounter)
 {
-    Token label;/*Hold label for validation*/
+    Token label;/*Holds label for validation*/
 
     /*Immediate number*/
     if(line[0] == '#'){
@@ -237,13 +268,11 @@ int verifyOperand(const char* line, const COMMANDS* cmd, int params, int* instCo
             lineCounter,
             cmd->cmdName,
             (params == 1) ? "1st" : "2nd");
+            return FALSE;
         }
         while(line[i] != '\0' && !isspace(line[i]) && line[i] != ',')
             ++i;
-        if(line[i] == '\0'){
-            printf("ERROR:%d: no label after '&' \n", lineCounter);
-            return FALSE;
-        }
+
         storeWord(&label, line+1, i-1);
         if(!isValidLabel(label.currentWord, label.len, lineCounter)){
             return FALSE;
